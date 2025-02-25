c2ea7c4e353f35fcb63ebc389c9d7102004cdebd
Kian Justine <kianjustinec@gmail.com>
2025-02-25 20:22:14 +0800

    first commit

--- assets/images/styles/scripts/main.js
+++
@@ -1,6 +0,0 @@
-const div = document.querySelector('div');
-const spanHI = document.querySelector("#hi");
-const spanBYE = document.querySelector("#bye");
-
-spanHI.style.color = "red";
-spanBYE.style.backgroundColor = "yellow";
